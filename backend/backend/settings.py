f4b7a0c15fa2ca7dcef6f4cab01a6fdba6bd8f85
Olivier DUVAL <zorky00@gmail.com>
2020-03-29 12:08:18 +0200

    mise à jour django 2.2.11 => django 3.0.1, djangorestframework-jwt (obsolète) 1.11.0 => drf-jwt 1.15.1, django-rest-swagger 2.2.0 (obsolète) => drf-yasg 1.17.1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     # 'corsheaders',
     'rest_framework',
     'rest_framework_jwt',
-    'rest_framework_swagger',
-
+    # 'rest_framework_swagger',
+    'drf_yasg',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,13 +19,32 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
+from rest_framework import permissions
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token, impersonate_jwt_token
-from rest_framework_swagger.views import get_swagger_view
+# from rest_framework_swagger.views import get_swagger_view
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-schema_view = get_swagger_view(title='Api plateforme')
+# schema_view = get_swagger_view(title='Api plateforme')
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Plateform APIs",
+      default_version='v1',
+      description="Plateform APIs",
+      terms_of_service="https://www.google.com/policies/terms/",
+      # contact=openapi.Contact(email="contact@snippets.local"),
+      # license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    url(r'^$', schema_view),
+    # url(r'^$', schema_view),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     url(r'^library/', include('api_library.urls')),
     url(r'^admin/', admin.site.urls)
 ]
