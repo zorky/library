5719b9030e9940c35bb68dc5df3dd3689efb0096
Olivier Duval <olivier.duval@univ-paris8.fr>
2020-05-10 17:23:45 +0200

    +common-library.module.ts

---
+++ frontend/library/src/app/common/common-library.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MaterialModule} from '../modules/material.module';
+import {FlexLayoutModule} from "@angular/flex-layout";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MaterialModule,
+  ],
+  declarations: [],
+  exports: [],
+})
+export class CommonLibraryModule { }
